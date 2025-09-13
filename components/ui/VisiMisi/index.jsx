2e634deb13b3044f34b206bcede846c2afbb5a4e
ainurrafi2123 <ainurrafi2123@gmail.com>
2025-09-13 08:42:34 +0700

    Update VisiMisi component layout and improve text formatting for better readability

--- components/ui/VisiMisi/index.jsx
+++ components/ui/VisiMisi/index.jsx
@@ -3,48 +3,61 @@ import Image from "next/image";
 
 const VisiMisi = () => {
     return (
-        <div className="h-[1100px] w-auto">
+        <div className="h-auto w-full">
             {/* Visi Kami */}
-            <div className="flex justify-between items-center px-10 py-10">
+            <div className="max-w-7xl mx-auto flex justify-between items-center px-6 lg:px-10 py-10">
                 <div>
-                    <h1 className="text-6xl font-bold pb-8">Visi Kami</h1>
-                    <p className="text-lg">Menjadi mitra utama pengguna global dalam<br/>menciptakan karya visual yang efektif dan<br/>menginspirasi melalui platform Canva.</p>
+                <h1 className="text-6xl font-bold pb-8">Visi Kami</h1>
+                <p className="text-lg">
+                    Menjadi mitra utama pengguna global dalam<br />
+                    menciptakan karya visual yang efektif dan<br />
+                    menginspirasi melalui platform Canva.
+                </p>
                 </div>
                 <div>
-                    <Image
-                    src={Vector}
-                    alt="Vector"
-                    height={350}
-                    />
+                    <Image src={Vector} alt="Vector" height={350} />
                 </div>
             </div>
+
             {/* Misi Kami */}
-            <div className="flex justify-between items-center p-10">
+            <div className="max-w-7xl mx-auto flex justify-between items-center px-6 lg:px-10 py-10">
                 <div>
                     <h1 className="text-6xl font-bold mb-[50px]">Misi Kami</h1>
-                    {/* Vector Image */}
                     <div>
-                        <Image
-                            src={Vector}
-                            alt="Vector"
-                            height={350}
-                        />
+                        <Image src={Vector} alt="Vector" height={350} />
                     </div>
                 </div>
                 <div className="bg-black w-[500px] h-auto flex flex-col gap-6 p-10 text-white">
                     <div>
                         <h1 className="text-xl font-semibold pb-3">Kualitas Template</h1>
-                        <p className="text-base">Menghasilkan template berkualitas tinggi yang<br/>inovatif dan relevan. Kami berkomitmen untuk terus<br/>memperbarui dan menyempurnakan koleksi<br/>template kami agar selalu selarasdengan tren<br/>desain terkini dan kebutuhan pasar.</p>
+                        <p className="text-base">
+                        Menghasilkan template berkualitas tinggi yang<br />
+                        inovatif dan relevan. Kami berkomitmen untuk terus<br />
+                        memperbarui dan menyempurnakan koleksi<br />
+                        template kami agar selalu selaras dengan tren<br />
+                        desain terkini dan kebutuhan pasar.
+                        </p>
                     </div>
-                    <hr />
-                    <div >
+                <hr />
+                    <div>
                         <h1 className="text-xl font-semibold pb-3">Mudah Digunakan</h1>
-                        <p className="text-base">Menyediakan pengalaman desain yang mudah diakses<br/>bagi pengguna Canva. Kami yakin bahwa setiap individu, tanpa memandang tingkat keterampilan desainnya, seharusnya bisa menghasilkan karya yang mengesankan dan profesional.</p>
+                        <p className="text-base">
+                        Menyediakan pengalaman desain yang mudah diakses<br />
+                        bagi pengguna Canva. Kami yakin bahwa setiap individu,
+                        tanpa memandang tingkat keterampilan desainnya,
+                        seharusnya bisa menghasilkan karya yang mengesankan
+                        dan profesional.
+                        </p>
                     </div>
-                    <hr />
+                <hr />
                     <div>
                         <h1 className="text-xl font-semibold pb-3">Fungsional</h1>
-                        <p className="text-base">Kami mendukung pengguna untuk memenuhi<br/>kebutuhan visual dengan desain fungsional dan<br/>menarik, memastikan setiap elemen dan desain<br/>efektif untuk konteks pribadi, bisnis, dan pendidikan.</p>
+                        <p className="text-base">
+                        Kami mendukung pengguna untuk memenuhi<br />
+                        kebutuhan visual dengan desain fungsional dan<br />
+                        menarik, memastikan setiap elemen dan desain<br />
+                        efektif untuk konteks pribadi, bisnis, dan pendidikan.
+                        </p>
                     </div>
                 </div>
             </div>
