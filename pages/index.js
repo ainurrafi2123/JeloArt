2960eb90bd8927a2841f8af87790fca6cd7b2f76
ainurrafi2123 <ainurrafi2123@gmail.com>
2025-09-13 15:34:35 +0700

    Refactor code structure for improved readability and maintainability

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Head from "next/head";
 import GradientWrapper from "../components/GradientWrapper";
 import Serve from "../components/ui/Serve";
@@ -8,24 +11,38 @@ import Slider from "../components/ui/Slider";
 import Testimonials from "../components/ui/Asset";
 import ToolKit from "../components/ui/Template";
 
+// import preloader
+import Preloader from "../components/ui/Preloader";
+
 export default function Home() {
+  const [complete, setComplete] = useState(false);
+
   return (
     <>
       <Head>
-        <meta name='robots' content='index' />
+        <meta name="robots" content="index" />
       </Head>
-      <Hero />
-      <Slider />
-      <AboutUs />
-      <VisiMisi />
-      <GradientWrapper>
-        <Serve />
-      </GradientWrapper>
-      <ToolKit />
-      <GradientWrapper>
-        <Testimonials />
-      </GradientWrapper>
-      {/* <FooterServe /> */}
+
+      {/* tampilkan Preloader dulu */}
+      {!complete && <Preloader setComplete={setComplete} />}
+
+      {/* setelah preloader selesai, konten muncul */}
+      {complete && (
+        <>
+          <Hero />
+          <Slider />
+          <AboutUs />
+          <VisiMisi />
+          <GradientWrapper>
+            <Serve />
+          </GradientWrapper>
+          <ToolKit />
+          <GradientWrapper>
+            <Testimonials />
+          </GradientWrapper>
+          {/* <FooterServe /> */}
+        </>
+      )}
     </>
   );
 }
