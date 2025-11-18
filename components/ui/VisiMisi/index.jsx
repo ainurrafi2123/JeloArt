3e843230e54a16d8a6c0cbe95ab4f77dca15fcb6
jaludwi <163502966+jaludwi@users.noreply.github.com>
2025-11-18 09:46:25 +0700

    Update Visi.jsx

--- components/ui/VisiMisi/index.jsx
+++ components/ui/VisiMisi/index.jsx
@@ -10,9 +10,7 @@ const VisiMisi = () => {
                 <div className="text-center md:text-left">
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold pb-6">Visi Kami</h1>
                     <p className="text-base sm:text-lg">
-                        Menjadi mitra utama pengguna global dalam<br />
-                        menciptakan karya visual yang efektif dan<br />
-                        menginspirasi melalui platform Canva.
+                       Menjadi mitra utama pengguna global dalam menciptakan karya visual yang efektif dan menginspirasi melalui pemanfaatan platform digital, sekaligus mendorong pertumbuhan ekosistem ekonomi kreatif lokal
                     </p>
                 </div>
                 <div className="flex justify-center">
