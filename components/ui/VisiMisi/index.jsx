358ce6d35cd5b611cc653cce18fb83e1a8208676
jaludwi <163502966+jaludwi@users.noreply.github.com>
2025-11-18 10:08:56 +0700

    Update index.jsx

--- components/ui/VisiMisi/index.jsx
+++ components/ui/VisiMisi/index.jsx
@@ -10,8 +10,8 @@ const VisiMisi = () => {
                 <div className="text-center md:text-left">
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold pb-6">Visi Kami</h1>
                     <p className="text-base sm:text-lg">
-                      Menjadi mitra utama pengguna global dalam menciptakan
-                        karya visual yang efektif dan menginspirasi melalui 
+                      Menjadi mitra utama pengguna global <br/> dalam menciptakan
+                        karya  <br/> visual yang efektif dan menginspirasi melalui 
                         pemanfaatan platform digital, sekaligus mendorong
                         pertumbuhan ekosistem ekonomi kreatif lokal 
                     </p>
